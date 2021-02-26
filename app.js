f275c73278f4400b122414c3571951c49f87a31c
nitishpccs89@gmail.com <nitishpccs89@gmail.com>
2021-02-26 21:35:32 +0530

    three commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello Azure</h1>');
+    res.end('<h1>Hello Azure Again</h1>');
 });
 server.listen(port,() => {
     console.log(`Server running at port `+port);
